75fa7d69fd3eb62e21bfa49a2c247c8c87671d77
BogdanMalashuk <kufar.120@gmail.com>
2025-08-20 00:54:12 +0300

    models registered in admin panel

--- services/core/apps/trades/admin.py
+++ services/core/apps/trades/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trade
 
-# Register your models here.
+
+@admin.register(Trade)
+class TradeAdmin(admin.ModelAdmin):
+    list_display = ("id", "profile", "exchange", "symbol", "side", "amount", "price", "timestamp")
+    list_filter = ("exchange", "side")
