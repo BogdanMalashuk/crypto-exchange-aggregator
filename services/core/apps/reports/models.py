7040bdf6eaa0b44323ef70a4bc15b45e93fd6f3c
BogdanMalashuk <kufar.120@gmail.com>
2025-08-19 17:07:34 +0300

    models import bug fixed

--- services/core/apps/reports/models.py
+++ services/core/apps/reports/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import Profile
+from apps.users.models import Profile
 
 
 class Report(models.Model):
--- services/core/apps/users/models.py
+++ services/core/apps/users/models.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
 class User(AbstractUser):
+
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
