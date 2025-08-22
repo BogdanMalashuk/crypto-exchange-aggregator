ec2735460f48f6bcb0e11140383ae1facc4c424a
BogdanMalashuk <kufar.120@gmail.com>
2025-08-22 14:19:30 +0300

    authorization views (including register, profile, api-keys etc.)

--- services/core/apps/users/views.py
+++ services/core/apps/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import RegisterSerializer, ProfileSerializer, ApiKeySerializer
+from .models import ApiKey
+from .permissions import ApiKeyAccessPermission
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
+
+
+class ApiKeyListCreateView(generics.ListCreateAPIView):
+    serializer_class = ApiKeySerializer
+    permission_classes = [permissions.IsAuthenticated, ApiKeyAccessPermission]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.profile.role == "admin":
+            return ApiKey.objects.select_related("profile", "profile__user").all()
+        return ApiKey.objects.filter(profile=user.profile)
+
+
+class ApiKeyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ApiKeySerializer
+    permission_classes = [permissions.IsAuthenticated, ApiKeyAccessPermission]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.profile.role == "admin":
+            return ApiKey.objects.select_related("profile", "profile__user").all()
+        return ApiKey.objects.filter(profile=user.profile)
