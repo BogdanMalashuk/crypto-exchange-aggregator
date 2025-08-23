e8b8eb9fb13ad4490cb26f51584137f254d96bb5
BogdanMalashuk <kufar.120@gmail.com>
2025-08-23 16:12:01 +0300

    u renamed into user

--- services/core/apps/users/permissions.py
+++ services/core/apps/users/permissions.py
@@ -3,10 +3,10 @@ from rest_framework.permissions import BasePermission
 
 class ApiKeyAccessPermission(BasePermission):
     def has_permission(self, request, view):
-        u = request.user
-        if not u or not u.is_authenticated or not hasattr(u, "profile"):
+        user = request.user
+        if not user or not user.is_authenticated or not hasattr(user, "profile"):
             return False
-        role = u.profile.role
+        role = user.profile.role
         if role == "analyst":
             return False
         return True
