39b94fcf2a72afc33fe7c1de46e49f299a259c9f
BogdanMalashuk <kufar.120@gmail.com>
2025-08-27 01:08:53 +0300

    key instance checker added

--- services/core/apps/users/serializers.py
+++ services/core/apps/users/serializers.py
@@ -40,13 +40,13 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 class ApiKeySerializer(serializers.ModelSerializer):
     api_secret = serializers.CharField(write_only=True)
+    api_secret_masked = serializers.SerializerMethodField()
 
     class Meta:
         model = ApiKey
         fields = ["id", "exchange", "api_key", "api_secret", "api_secret_masked", "created_at"]
         read_only_fields = ["id", "created_at", "api_secret_masked"]
 
-    @staticmethod
     def get_api_secret_masked(self, obj) -> str:
         secret = decrypt_text(obj.api_secret)
         return ("***" + secret[-4:]) if secret else "***"
@@ -54,10 +54,8 @@ class ApiKeySerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         request = self.context["request"]
         profile = request.user.profile
-
         raw_secret = validated_data.pop("api_secret")
         encrypted_secret = encrypt_text(raw_secret)
-
         try:
             instance = ApiKey.objects.create(
                 profile=profile,
@@ -76,5 +74,10 @@ class ApiKeySerializer(serializers.ModelSerializer):
         for field in ("exchange", "api_key"):
             if field in validated_data:
                 setattr(instance, field, validated_data[field])
-        instance.save()
+        try:
+            instance.save()
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {"exchange": "API key for this exchange already exists for your profile."}
+            )
         return instance
