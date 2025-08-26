a21f3227c98bec9c9e4599348e15d0fb5782fde2
BogdanMalashuk <kufar.120@gmail.com>
2025-08-27 01:08:24 +0300

    permissions updates

--- services/core/apps/users/permissions.py
+++ services/core/apps/users/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from .models import Profile
 
 
 class ApiKeyAccessPermission(BasePermission):
@@ -7,6 +8,12 @@ class ApiKeyAccessPermission(BasePermission):
         if not user or not user.is_authenticated or not hasattr(user, "profile"):
             return False
         role = user.profile.role
-        if role == "analyst":
+        if role == Profile.Role.ANALYST:
             return False
         return True
+
+    def has_object_permission(self, request, view, obj):
+        role = request.user.profile.role
+        if role == Profile.Role.ADMIN:
+            return True
+        return obj.profile == request.user.profile
