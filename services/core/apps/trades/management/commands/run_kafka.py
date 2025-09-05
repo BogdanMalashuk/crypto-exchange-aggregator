9bfb90ad35a4cf0147b0644ac6e4aa396cf7fe17
BogdanMalashuk <kufar.120@gmail.com>
2025-09-05 14:09:10 +0300

    command deleted

--- services/core/apps/trades/management/commands/run_kafka.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from apps.trades.consumer import run_consumer
-
-
-class Command(BaseCommand):
-    help = "Run Kafka consumer for trade.profit.detected"
-
-    def handle(self, *args, **options):
-        run_consumer()
