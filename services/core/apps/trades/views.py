71c6d225cc112b2ccab58c7a8f3e7c47ee94c110
BogdanMalashuk <kufar.120@gmail.com>
2025-09-06 13:23:16 +0300

    mark_sold method deletedv

--- services/core/apps/trades/views.py
+++ services/core/apps/trades/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .models import Trade
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils import timezone
+from rest_framework import viewsets, status
+from .serializers import TradeSerializer
 
-# Create your views here.
+
+class TradeViewSet(viewsets.ModelViewSet):
+    queryset = Trade.objects.all()
+    serializer_class = TradeSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user_id = self.request.query_params.get("user_id")
+        symbol = self.request.query_params.get("symbol")
+        sold = self.request.query_params.get("sold")
+        if user_id:
+            qs = qs.filter(user_id=user_id)
+        if symbol:
+            qs = qs.filter(symbol__iexact=symbol)
+        if sold is not None:
+            qs = qs.filter(sold=(sold.lower() == "true"))
+        return qs
