b1a125b09ef1f9acd5f91ed2ee6765470461fcb2
BogdanMalashuk <kufar.120@gmail.com>
2025-09-06 13:19:48 +0300

    admin registering deleted, will be updated soon...

--- services/core/apps/trades/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Trade
-
-
-@admin.register(Trade)
-class TradeAdmin(admin.ModelAdmin):
-    list_display = ("id", "profile", "exchange", "symbol", "side", "amount", "price", "timestamp")
-    list_filter = ("exchange", "side")
