5f2d0a936152b8c3ea858666142cd909fc94eb34
BogdanMalashuk <kufar.120@gmail.com>
2025-09-16 18:36:42 +0300

    router refactoring & bugfixes

--- services/core/apps/users/urls.py
+++ services/core/apps/users/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView, ApiKeyListCreateView, ApiKeyDetailView
+from .views import RegisterView, ApiKeyListCreateView, ApiKeyDetailView, UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("profile/", ProfileView.as_view(), name="profile"),
+    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
-    path("apikeys/", ApiKeyListCreateView.as_view(), name="apikey-list-create"),
-    path("apikeys/<int:pk>/", ApiKeyDetailView.as_view(), name="apikey-detail"),
+    path("keys/", ApiKeyListCreateView.as_view(), name="apikey-list-create"),
+    path("keys/<int:pk>/", ApiKeyDetailView.as_view(), name="apikey-detail"),
 ]
+
+urlpatterns += router.urls
