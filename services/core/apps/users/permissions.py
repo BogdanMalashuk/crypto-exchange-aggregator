c84cc4cb180955d55cb2764cc91b6cc2b2a0414e
BogdanMalashuk <kufar.120@gmail.com>
2025-09-16 18:38:12 +0300

    user management permissions, updating

--- services/core/apps/users/permissions.py
+++ services/core/apps/users/permissions.py
@@ -1,19 +1,27 @@
 from rest_framework.permissions import BasePermission
-from .models import Profile
 
 
 class ApiKeyAccessPermission(BasePermission):
     def has_permission(self, request, view):
         user = request.user
-        if not user or not user.is_authenticated or not hasattr(user, "profile"):
+        if not user or not user.is_authenticated:
             return False
-        role = user.profile.role
-        if role == Profile.Role.ANALYST:
+        if user.role == "analyst":
             return False
         return True
 
     def has_object_permission(self, request, view, obj):
-        role = request.user.profile.role
-        if role == Profile.Role.ADMIN:
+        user = request.user
+        if user.role == user.Role.ADMIN:
             return True
-        return obj.profile == request.user.profile
+        return obj.user == user
+
+
+class UserManagementPermission(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        if not user or not user.is_authenticated:
+            return False
+        if request.method in ("POST", "PUT", "PATCH", "DELETE"):
+            return user.role == user.Role.ADMIN
+        return True
