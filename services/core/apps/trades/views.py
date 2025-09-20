7b92ad688b64a76e1a46ddcdcb2d05f18aab324f
BogdanMalashuk <kufar.120@gmail.com>
2025-09-20 15:04:08 +0300

    permissions added, 403 for other-users reports

--- services/core/apps/trades/views.py
+++ services/core/apps/trades/views.py
@@ -1,6 +1,8 @@
-from .models import Trade
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from .models import Trade
 from .serializers import TradeSerializer
+from apps.users.models import User
 
 
 class TradeViewSet(viewsets.ModelViewSet):
@@ -9,13 +11,23 @@ class TradeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         qs = super().get_queryset()
+        user = self.request.user
+
         user_id = self.request.query_params.get("user_id")
         symbol = self.request.query_params.get("symbol")
         sold = self.request.query_params.get("sold")
-        if user_id:
-            qs = qs.filter(user_id=user_id)
+
+        if self.request.user.role == User.Role.USER:
+            if user_id and str(user_id) != str(user.id):
+                raise PermissionDenied("You don't have permissions for this action.")
+            qs = qs.filter(user_id=user.id)
+        else:
+            if user_id:
+                qs = qs.filter(user_id=user_id)
+
         if symbol:
             qs = qs.filter(symbol__iexact=symbol)
         if sold is not None:
             qs = qs.filter(sold=(sold.lower() == "true"))
+
         return qs
