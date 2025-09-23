025effa112a55438407405c19149aff30415a7d7
BogdanMalashuk <kufar.120@gmail.com>
2025-09-23 14:19:30 +0300

    serializers for password reset

--- services/core/apps/users/serializers.py
+++ services/core/apps/users/serializers.py
@@ -91,3 +91,13 @@ class ApiKeySerializer(serializers.ModelSerializer):
                 {"exchange": "API key for this exchange already exists for your user."}
             )
         return instance
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+    new_password = serializers.CharField(min_length=8)
